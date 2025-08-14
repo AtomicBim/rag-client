c9f3d14dcaf8dc228769ac3d7f308b410903b5a8
AtomicBim <romangman90@gmail.com>
2025-08-14 14:43:07 +0500

    +

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 QDRANT_HOST = "192.168.42.188"
 QDRANT_PORT = 6333
 COLLECTION_NAME = "internal_regulations_v2"
-EMBEDDING_MODEL_PATH = "C:/Users/r.grigoriev/Desktop/rag-client/local_model/ru-en-RoSBERTa"
+EMBEDDING_MODEL_PATH = "C:/Users/r.grigoriev/Desktop/rag-client/local_model/sbert_large_mt_nlu_ru"
 EMBEDDING_DIMENSION = 1024
 OPENAI_API_ENDPOINT = "http://192.168.45.79:8000/generate_answer"
 DOCS_ROOT_PATH = "./rag-source"
